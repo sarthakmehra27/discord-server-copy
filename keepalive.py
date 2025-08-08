c0a4a1ae48d94d67619be03f47666d27af03183a
sarthakmehra27 <72352381+sarthakmehra27@users.noreply.github.com>
2025-08-09 01:52:48 +0530

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -13,7 +13,8 @@ def ping():
     return "pong", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Use env port or fallback to 8080
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
